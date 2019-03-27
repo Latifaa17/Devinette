2e62732ce532fd652601a9df4bf91fd196dc6be0
latifa <latifaadhibi1@gmail.com>
2019-03-27 21:31:30 +0100

    adding  a menu

--- JS/exercice.js
+++ JS/exercice.js
@@ -1,33 +1,38 @@
 
 function jouer() {
-    var nb=Math.floor(Math.random() * 100) + 1;
-    var test=false;
-    var i=0;
-    while(i<8 && !test)
+    var choix=1;
+    while(choix==1)
     {
-        var n = prompt("choisissez un entier:");
-        if(n==nb)
+        var nb=Math.floor(Math.random() * 100) + 1;
+        var test=false;
+        var i=0;
+        while(i<8 && !test)
+        {
+            var n = prompt("choisissez un entier:");
+            if(n==nb)
+                {
+                    console.log("bravo!");
+                    console.log("la réponse correcte est:"+n);
+                    console.log("vous avez gagné à la tentative numero "+(i+1));
+                    test=true;
+                }
+            else if(n<nb)
+                {
+                    console.log("l'entier à chercher est plus grand que "+n);
+                    console.log("il vous reste "+(7-i)+" tentatives");
+                }
+            else 
             {
-                console.log("bravo!");
-                console.log("la réponse correcte est:"+n);
-                console.log("vous avez gagné à la tentative numero "+(i+1));
-                test=true;
-            }
-        else if(n<nb)
-            {
-                console.log("l'entier à chercher est plus grand que "+n);
+                console.log("l'entier à chercher est plus petit que "+n);
                 console.log("il vous reste "+(7-i)+" tentatives");
             }
-        else 
-        {
-            console.log("l'entier à chercher est plus petit que "+n);
-            console.log("il vous reste "+(7-i)+" tentatives");
+            i+=1;
         }
-        i+=1;
-    }
-    if(!test)
-    {
-        console.log("vous avez perdu :( ");
-        console.log("la réponse correcte est: "+nb);
+        if(!test)
+        {
+            console.log("vous avez perdu :( ");
+            console.log("la réponse correcte est: "+nb);
+        }
+        choix=prompt("Voulez-vous rejouer? 1:oui 2:non");
     }
   }
