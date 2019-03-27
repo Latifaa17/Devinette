b5eb2599ce4f748b87dbd641dad3cb8bc66e67eb
latifa <latifaadhibi1@gmail.com>
2019-03-27 20:53:58 +0100

    implémentation de la fonction jouer

---
+++ JS/exercice.js
@@ -0,0 +1,32 @@
+
+function jouer() {
+    var nb=Math.floor(Math.random() * 100) + 1;
+    var test=false;
+    var i=0;
+    while(i<8 && !test)
+    {
+        var n = prompt("choisissez un entier:");
+        if(n==nb)
+            {
+                console.log("bravo!");
+                console.log("vous avez gagné à la tentative numero "+(i+1));
+                test=true;
+            }
+        else if(n<nb)
+            {
+                console.log("l'entier à chercher est plus grand que "+n);
+                console.log("il vous reste "+(7-i)+" tentatives");
+            }
+        else 
+        {
+            console.log("l'entier à chercher est plus petit que "+n);
+            console.log("il vous reste "+(7-i)+" tentatives");
+        }
+        i+=1;
+    }
+    if(!test)
+    {
+        console.log("vous avez perdu :( ");
+        console.log("l'entier à chercher était "+nb);
+    }
+  }
