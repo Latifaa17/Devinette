26592b382797ff1a8f54363ad44a9861e1dfa643
latifa <latifaadhibi1@gmail.com>
2019-03-29 11:43:10 +0100

    adding isNaN condition

--- JS/exercice.js
+++ JS/exercice.js
@@ -6,28 +6,31 @@ function jouer() {
         var nb=Math.floor(Math.random() * 100) + 1;
         var test=false;
         var i=0;
-        while(i<8 && !test)
-        {
-            var n = prompt("choisissez un entier:");
-            if(n==nb)
+        do
+        { 
+            var n = Number(prompt("choisissez un entier:"));
+            if(!isNaN(n))
+            {
+                if(n==nb)
+                    {
+                        console.log("bravo!");
+                        console.log("la réponse correcte est:"+n);
+                        console.log("vous avez gagné à la tentative numero "+(i+1));
+                        test=true;
+                    }
+                else if(n<nb)
+                    {
+                        console.log("l'entier à chercher est plus grand que "+n);
+                        console.log("il vous reste "+(7-i)+" tentatives");
+                    }
+                else 
                 {
-                    console.log("bravo!");
-                    console.log("la réponse correcte est:"+n);
-                    console.log("vous avez gagné à la tentative numero "+(i+1));
-                    test=true;
-                }
-            else if(n<nb)
-                {
-                    console.log("l'entier à chercher est plus grand que "+n);
+                    console.log("l'entier à chercher est plus petit que "+n);
                     console.log("il vous reste "+(7-i)+" tentatives");
                 }
-            else 
-            {
-                console.log("l'entier à chercher est plus petit que "+n);
-                console.log("il vous reste "+(7-i)+" tentatives");
+                i+=1;
             }
-            i+=1;
-        }
+        }while(i<8 && !test)
         if(!test)
         {
             console.log("vous avez perdu :( ");
